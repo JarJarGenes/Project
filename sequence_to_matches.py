4dd43e114e38f6c15f2d45e34e34dc094d656d53
dmontielg <diegoomontiel@gmail.com>
2015-12-03 09:44:02 +0100

    transdecoder and blastp added

--- sequence_to_matches.py
+++ sequence_to_matches.py
@@ -17,38 +17,48 @@ def jarjargenes():
     out = False   
     while not out:
         input_number_sequence = raw_input("How many sequences do you want "+\
-        "from your fasta file? (Press 0 to exit): ")
+        "from your fasta file? (Press -1 to sequence the whole sequence" \
+         "Press 0 to exit the program\n Warning! this may take a while"+\
+         "depending on your file size!): ")
         if input_number_sequence > '0':
-
             flow_program(int(input_number_sequence))
             break
-        elif input_number_sequence == '0':
-            print "--- Thanks for using JarJar for getting small "+\
-            "fasta datasets---"
-            out = True
+        elif input_number_sequence < '0':
+            flow_program(0)
+            break            
+        else:
+            print "#################################################" 
+            print "Thanks for using this program, Beta version V1.0" 
+            print "#################################################"             
+            break
 
 def flow_program(input_number_sequence):
     #Follows the main flow of the program
     #open the two files from the user for getting the N sequence
     small_fasta_file = ''
-    fasta_filename  = ''
-    gff_filename    = ''
+    protein_file_cat = ''    
+    scaffold_file  = ''
     with open(argv[1]) as fh:
-        fasta_filename    = fh.name
-        index             = fasta_filename.rfind('/')
-        fasta_filename    = fasta_filename[index:].replace('/','')+'.small'
-        small_fasta_file  = small_fasta_dataset(fh,fasta_filename,\
+        scaffold_file    = fh.name
+        index             = scaffold_file.rfind('/')
+        scaffold_file    = scaffold_file[index:].replace('/','')
+        small_fasta_file  = small_fasta_dataset(fh,scaffold_file,\
         input_number_sequence)
-    
+    """
     with open(argv[2]) as fh:
         gff_filename    = fh.name
         index           = gff_filename.rfind('/')
         gff_filename    = gff_filename[index:].replace('/','')+'.small'
         small_gff_dataset(fh,small_fasta_file,gff_filename)
-        
-    protein_file = gff_to_protein(gff_filename,fasta_filename)
-
+    """
     
+    with open(argv[2]) as fh:
+        gtf_filename     = fh.name
+        #index            = gtf_filename.rfind('/')
+        #gtf_filename     = gtf_filename[index:].replace('/','')+'.small'
+        protein_file_cat = gtf_to_protein(scaffold_file,gtf_filename)
+    print protein_file_cat
+ 
     cmd = 'clear'
     print subprocess.check_output(cmd, shell=True)
     print "###########################################"
@@ -56,9 +66,9 @@ def flow_program(input_number_sequence):
     print "###########################################"    
     cmd = 'ls *.fasta'
     print subprocess.check_output(cmd, shell=True)
-    protein_file_genes = \
+    protein_file_arabidopsis = \
     raw_input("Please select the proteins file to blast: ")    
-    blastp(protein_file,protein_file_genes)
+    blastp(protein_file_cat,protein_file_arabidopsis)
     
     
 def small_fasta_dataset(input_file,small_fasta_file,input_number_sequence):
@@ -102,27 +112,43 @@ def small_fasta_dataset(input_file,small_fasta_file,input_number_sequence):
             newlist = sorted(dictionary_sequence,reverse=True, \
             key=lambda k: k['lenght'])     
             outfile = open(small_fasta_file,'w')    
-        
-            for index in range(input_number_sequence):    
-                outfile.write(str(newlist[index]['name']))
-                outfile.write(str(newlist[index]['sequence']))                            
-            outfile.close()
-            print "File {0} succesfully generated!"\
-               .format(small_fasta_file)
-        
+            
+            if (input_number_sequence != 0):            
+            
+                for index in range(input_number_sequence):    
+                    outfile.write(str(newlist[index]['name']))
+                    outfile.write(str(newlist[index]['sequence']))                            
+                outfile.close()
+                print "File {0} succesfully generated!"\
+                   .format(small_fasta_file)
+            else:
+                for index in range(len(newlist)):    
+                    outfile.write(str(newlist[index]['name']))
+                    outfile.write(str(newlist[index]['sequence']))                            
+                outfile.close()
+                print "File {0} succesfully generated!"\
+                   .format(small_fasta_file)
         else:
             pass
     else:
         newlist = sorted(dictionary_sequence,reverse=True, \
         key=lambda k: k['lenght'])     
         outfile = open(small_fasta_file,'w')    
-        
-        for index in range(input_number_sequence):    
-            outfile.write(str(newlist[index]['name']))
-            outfile.write(str(newlist[index]['sequence']))                            
-        outfile.close()
-        print "File {0} succesfully generated!"\
-        .format(small_fasta_file)
+        if (input_number_sequence != 0):    
+            
+            for index in range(input_number_sequence):    
+                outfile.write(str(newlist[index]['name']))
+                outfile.write(str(newlist[index]['sequence']))                            
+            outfile.close()
+            print "File {0} succesfully generated!"\
+            .format(small_fasta_file)
+        else:
+            for index in range(len(newlist)):    
+                outfile.write(str(newlist[index]['name']))
+                outfile.write(str(newlist[index]['sequence']))                            
+            outfile.close()
+            print "File {0} succesfully generated!"\
+            .format(small_fasta_file)
     return small_fasta_file
     
 def small_gff_dataset(input_gff_file,small_fasta_file,small_gff_filename):    
@@ -171,29 +197,48 @@ def small_gff_dataset(input_gff_file,small_fasta_file,small_gff_filename):
     
     return small_gff_filename
         
-def gff_to_protein(gff_file, genome_db):
+def gtf_to_protein(scaffold_file,gtf_filename):
     """This is a function that needs a gff file and a genome file and 
     returns proteins
     """
-    protein_file = 'protein_file_cat.fasta'
-    print "Debugging..."
-    print gff_file
-    print genome_db
+    init = gtf_filename.index('.') 
+    gff_filename = gtf_filename[:init]+'.gff3'    
+    #TransDecoder-2.0/util/cufflinks_gtf_to_alignment_gff3.pl 
+    #                   transcripts.gtf > transcripts.gff3    
+    cmd = 'TransDecoder-2.0/util/cufflinks_gtf_to_alignment_gff3.pl' \
+    " %s > %s" % (gtf_filename, gff_filename )  
+    subprocess.check_output(cmd, shell=True)
+    #Name for the protein file from the transdecoder
+    protein_file_cat = 'protein_file_cat.fasta'
+    print "Running..."
+    print "Please wait..."
+    #Problems in the protein_file_Cat.fasta
+    print scaffold_file
     cmd = 'TransDecoder-2.0/util/gff3_file_to_proteins.pl {0} {1} > {2}'\
-    .format(gff_file, genome_db, protein_file)
+    .format(gff_filename, scaffold_file, protein_file_cat)
     subprocess.check_output(cmd, shell=True)
-    return protein_file        
+
+    return protein_file_cat        
+    
         
-        
-def blastp(protein_file_cat, protein_file_genes):
+def blastp(protein_file_cat, protein_file_arabidopsis):
     """This function needs a file with protein sequences and returns a 
-    file with matche Let op Evalue 1E-5 en coverage! 
+    file with matches Let op Evalue 1E-5 en coverage! 
     """
-    cmd = 'makeblastdb -in %s'% (protein_file_genes)
-    matches_file = 'matches.txt'
-    cmd = 'blastp -query %s -db %s -out %s -evalue 1E-5' \
-    % (protein_file_cat.fasta, protein_file_genes, matches_file)
+    init = protein_file_arabidopsis.index('.') 
+    database = 'db_proteins_'+protein_file_arabidopsis[:init]
+    #makeblastdb -in protein_file_arabidopsis.fasta -dbtype prot 
+    #-out db_protein_file_genes.db
+    cmd = 'makeblastdb -in %s -dbtype prot -out %s  '\
+    % (protein_file_arabidopsis,database)
     subprocess.check_output(cmd, shell=True)
+    matches_file = 'matches.txt'
+    #blastp -query protein_file_cat.fasta -db db_protein_file_genes.db 
+    #-out matches.txt -outfmt 7 -evalue 1E-5
+    cmd = 'blastp -query %s -db %s -out %s -outfmt 7 -evalue 1E-5' \
+    % (protein_file_cat, database, matches_file)
+    subprocess.check_output(cmd, shell=True)
+    print "Done."
     return matches_file        
         
 if __name__== "__main__":
